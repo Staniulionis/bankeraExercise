b7684a320b1e0507983dcf073bdf7058404ddea9
Staniulionis <136055644+Staniulionis@users.noreply.github.com>
2023-08-09 09:33:49 +0300

    Update spec.cy.js

--- spec.cy.js
+++ spec.cy.js
@@ -11,9 +11,9 @@ it("Spectrocoin test", () => {
   cy.get('.css-1836tev').should('have.text', 'EUR')
   cy.get('tr:nth-child(1) td:nth-child(3)').then(($element) => {
     if ($element.hasClass('MobileTable_green__uQizf')) {
-      cy.wrap($element).should('contain.text', '+')
+      cy.wrap($element).should('have.css', 'color', 'rgb(83, 199, 137)')
     } else if ($element.hasClass('MobileTable_red__robxd')) {
-      cy.wrap($element).should('contain.text', '-')
+      cy.wrap($element).should('have.css', 'color', 'rgb(221, 75, 100)')
     }
   })
 })
